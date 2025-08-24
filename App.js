7b200d2dd7225906279781748aa924e032d9dddf
evaanbailleul59 <evaanbailleul@gmail.com>
2025-08-24 05:22:57 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚚 Bienvenue sur mon appli de livraison !</h1>
+      <p>Version test déployée sur Vercel.</p>
+    </div>
+  );
+  }
